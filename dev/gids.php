88e29702820442f45e76664bcd150753f336c2f5
Wesley Lolkema <wesley@lolkema.net>
2015-10-17 00:33:19 +0200

    Zoeken aangepast en suggesties toegevoegd
    Het zoeken is aangepast en er komen nu suggesties tevoorschijn bij het
    typen

--- dev/gids.php
+++ dev/gids.php
@@ -1,364 +1,213 @@
 <div id="geo" class="geolocation_data"></div>
-<script type="text/JavaScript" src="./geolocation/geo.js"></script>
+<script type="text/javascript" src="./geolocation/geo.js"></script>
+
 <?php
-
 require('./controllers/header.php');
+$distance = 100;
 
-$trefwoord = null;
+// Pagination
+$nummer = 0;
 
-if(isset($_POST['Zoek']))
-{
-	unset($_GET);
-	unset($_SESSION['trefwoord']);
+if(isset($_GET['nr'])) {
+	$nummer = $_GET['nr'];
 }
-if(isset($_GET['nr']))
-{
-$nr = $_GET['nr']; 
-$start = $nr * 50;
-$lat = $_SESSION['lat'];
-$lon = $_SESSION['lon'];
-$trefwoord = $_SESSION['trefwoord'];
-$afstand = $_SESSION['afstand'];
-$_POST['Zoek'] = 1;
+
+if($nummer == 0 && !isset($_GET['nr'])) {
+	session_unset();
+}
+
 $range = 50;
-}
-else
-{
-$start = 0;
-$range = 50;
-$_GET['nr'] = 0;
-	if(isset($_POST['afstand'])){
-	$afstand = $_POST['afstand'];}
+$start = $nummer * $range;
 
-	if(isset($_POST['trefwoord'])){
-	$trefwoord = $_POST['trefwoord'];}
+// Check users lat & long
+if(isset($_SESSION['lat']) && isset($_SESSION['lon'])) {
+	$latitude = $_SESSION['lat'];
+	$longitude = $_SESSION['lon'];
+} else {
+	$latitude = null;
+	$longitude = null;
 }
 
-
-if(isset($_POST['Zoek']))
-{
-	$city = NULL;
-	$checkcity = NULL;
-	if (!isset($_SESSION['trefwoord'])){
-	$trefwoord = NULL;}
-
-	
-	if(isset($_POST['trefwoord']))
-	{
+// Check if "trefwoord" exists
+if(isset($_POST['trefwoord'])) {
 	$trefwoord = $_POST['trefwoord'];
-			
-	}		
-	$sth = $pdo->prepare('SELECT DISTINCT plaats FROM bedrijfgegevens WHERE MATCH (plaats) AGAINST ("'.$trefwoord.'" IN BOOLEAN MODE)');
-	$sth->execute();
-	$row = $sth->fetch(PDO::FETCH_ASSOC);
-	$city = $row['plaats'];
-	
-	if( ! $row)
-	{
-	if(!empty($_SESSION['lat'])){$lat = $_SESSION['lat'];}
-	if(!empty($_SESSION['lon'])){$lon = $_SESSION['lon'];}
-	}
-	else
-	{	
-	$url = 'https://api.pro6pp.nl/v1/suggest?auth_key=9mPiDJVEjKZliA8I&per_page=1&nl_city='.$city.'&format=json';
-
-	$response = file_get_contents($url);
- 
-	$json = json_decode($response,TRUE); 
- 
-	$city = $json['results'][0]['lat'].",".$json['results'][0]['lng'];
-	
-
-	
-	$coords = (explode(",",$city));
-	$lat = $coords[0];
-	$lon = $coords[1];
-	$_SESSION['lat'] = $lat;
-	$_SESSION['lon'] = $lon;
-	echo 'test2';
-	}	
+	$_SESSION['trefwoord'] = $trefwoord;
+} elseif($_SESSION['trefwoord']) {
+	$trefwoord = $_SESSION['trefwoord'];
+} else {
+	$trefwoord = null;
 }
-
-
 ?>
 
-
-<form id="opnaam" method="post" action="gids.php">
-
-	<div class="zoeken">
-		<div class="row">
-			
-			<div class="col-sm-12 col-md-12 filter2">
+<div class="zoeken">
+	<div class="row">
+		<div class="col-sm-12 col-md-12 filter2">
+			<form id="opnaam" method="post" action="gids.php">
 				<div class="col-xs-9 col-sm-9 col-md-6">
-					<input class="form-control" type="text" name="trefwoord" placeholder="Trefwoord" autofocus size="20" value="<?php echo $trefwoord; ?>" >
+					<input class="form-control" type="text" name="trefwoord" placeholder="Trefwoord" autofocus size="20" value="<?php echo $trefwoord; ?>" />
 				</div>
-				<div class="col-xs-3 col-sm-3 col-md-2">
-				<select class="form-control search-select col-xs-12 col-sm-4" id="sel1" name="afstand">
-					
-					<option value="1000">Alle afstanden</option>
-					<option value="3">< 3 km</option>
-					<option value="5">< 5 km</option>
-					<option value="10">< 10 km</option>
-					<option value="15">< 15 km</option>
-					<option value="25">< 25 km</option>
-					<option value="50">< 50 km</option>
-					<option value="75">< 75 km</option>
-					<option value="100">< 100 km</option>
-					<option value="150">< 150 km</option>
-					
-	
-				</select>		
-			</div>
-				<div class=" col-xs-6 col-sm-6 col-md-2 xs-pull-right">
-					<button class="btn btn-default col-xs-12 " type="submit" name="Zoek" value="Zoek">Zoek</button>
+
+				<div class="col-xs-6 col-sm-6 col-md-2 xs-pull-right">
+					<button class="btn btn-default col-xs-12" type="submit" name="Zoek" value="Zoek">Zoek</button>
 				</div>
+
 				<div class="col-xs-6 col-sm-6 col-md-2 xs-pull-left">
-					<a class="btn btn-default col-xs-6 col-sm-12" href="?paginanr=3">Reset</a>
+					<a class="btn btn-default col-xs-6 col-sm-12" href="?nr=0">Reset</a>
 				</div>
-			</div>
+			</form>
 		</div>
 	</div>
-</form>
- 
+</div>
+
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'POST' || isset($_GET['nr'])) {
+	$select = "SELECT
+				bedrijfgegevens.id,
+				bedrijfgegevens.bedrijfsnaam,
+				bedrijfgegevens.telefoonnummer,
+				bedrijfgegevens.mobielnummer,
+				bedrijfgegevens.premium,
+				bedrijfgegevens.logo";
 
-if(isset($_POST['Zoek']))
-{
+	if(isset($latitude) && isset($longitude)) {
+		$select .= ", (6371
+					* acos(cos(radians(" . $latitude . "))
+					* cos(radians(bedrijfgegevens.latitude))
+					* cos(radians(bedrijfgegevens.longitude)
+					- radians(" . $longitude . "))
+					+ sin (radians(" . $latitude . "))
+					* sin (radians(bedrijfgegevens.latitude)))
+					) AS distance";
+	}
 
-	//De zoek query
-	$search = NULL;
-	if(!empty($trefwoord))
-		{
-		$trefwoorden = (explode(" ",$trefwoord));
-		//var_dump($trefwoorden);
-		
-		foreach ($trefwoorden as $value2)
-			{	
-				if(!empty($value2))
-				{
-					$search.= ''.$value2.'* ';
-				}
-			}
+	$query = $select . " FROM
+							bedrijfgegevens
+						INNER JOIN
+							subbranches on subbranches.id = bedrijfgegevens.subbranche_id
+						INNER JOIN
+							branches on branches.id = subbranches.branche_id
+						WHERE
+							branches.naam = :branche
+						OR
+							subbranches.naam = :subbranche
+						";
+
+	$companyQuery = $select . " FROM
+									bedrijfgegevens
+								WHERE
+									locate('" . $trefwoord . "', bedrijfsnaam)
+								UNION " . $select . "
+								FROM
+									specialiteiten
+								INNER JOIN
+									bedrijfgegevens_specialiteiten on bedrijfgegevens_specialiteiten.specialiteiten_id = specialiteiten.id
+								INNER JOIN
+									bedrijfgegevens on bedrijfgegevens.id = bedrijfgegevens_specialiteiten.bedrijfgegevens_id
+								WHERE
+									locate('" . $trefwoord . "', specialiteiten.naam)
+								";
+
+
+
+	if(!empty($trefwoord)) {
+		if (strpos($trefwoord, '>')) {
+			$branche = substr($trefwoord, 0, strrpos($trefwoord, ' >'));
+			$subbranche = substr($trefwoord, strrpos($trefwoord, '> ') + 2);
+		} else {
+			$branche = $trefwoord;
+			$subbranche = $trefwoord;
 		}
-	
 
-	
-	
-	if(!isset($lat) and !isset($lon) and $search != '')
-	{
-		$sth = $pdo->prepare('
-		SELECT
-		bedrijfgegevens.bedrijfsnaam,
-		bedrijfgegevens.provincie,
-		bedrijfgegevens.postcode,
-		bedrijfgegevens.plaats,
-		bedrijfgegevens.branche,
-		bedrijfgegevens.premium,
-		bedrijfgegevens.telefoon,
-		bedrijfgegevens.logo,
-		bedrijfs_specialiteiten.*
-	FROM 
-		bedrijfgegevens
-	INNER JOIN
-		bedrijfs_specialiteiten on bedrijfgegevens.bedrijfs_id = bedrijfs_specialiteiten.bedrijfs_id 
-	WHERE 
-		MATCH (bedrijfgegevens.bedrijfsnaam, bedrijfgegevens.postcode, bedrijfgegevens.plaats, bedrijfgegevens.provincie, bedrijfgegevens.branche)
-		AGAINST ("'.$search.'" IN BOOLEAN MODE) 
-		OR MATCH (bedrijfs_specialiteiten.specialiteit_1)
-		AGAINST ("'.$search.'" IN BOOLEAN MODE)
-		LIMIT '.$start.','.$range
-		);	
+		if(!isset($latitude) && !isset($longitude)) {
+			$limit = " LIMIT " . $start . "," . ($range+1);
+		} elseif(isset($latitude) && isset($longitude)) {
+			$limit = " HAVING
+						distance < " . $distance . "
+					ORDER BY
+						distance
+					LIMIT " . $start . " , " . ($range+1);
+		}
+
+		$sth = $pdo->prepare($query.$limit);
+
+		$sth->bindValue(':branche', $branche, PDO::PARAM_STR);
+		$sth->bindValue(':subbranche', $subbranche, PDO::PARAM_STR);
+		$sth->execute();
+
+		if($sth->rowCount() == 0) {
+			$sth = $pdo->prepare($companyQuery.$limit);
+			$sth->execute();
+		}
 	}
-	
-	elseif(isset($lat) and isset($lon) and $search == '')
-	{
-		
-		
-		$sth = $pdo->prepare('
-		SELECT
-		bedrijfgegevens.bedrijfsnaam,
-		bedrijfgegevens.provincie,
-		bedrijfgegevens.postcode,
-		bedrijfgegevens.plaats,
-		bedrijfgegevens.branche,
-		bedrijfgegevens.premium,
-		bedrijfgegevens.telefoon,
-		bedrijfgegevens.logo,
-		(6371
-		* acos(cos(radians('.$lat.'))
-		* cos(radians(latitude))
-		* cos(radians(longitude)
-		- radians('.$lon.'))
-		+ sin(radians('.$lat.'))
-		* sin(radians(latitude))) 
-		) AS distance,
-		bedrijfs_specialiteiten.*
-	FROM 
-		bedrijfgegevens
-	INNER JOIN
-		bedrijfs_specialiteiten on bedrijfgegevens.bedrijfs_id = bedrijfs_specialiteiten.bedrijfs_id 
-		HAVING distance < '.$afstand.'
-		ORDER BY distance ASC
-		LIMIT '.$start.','.$range
-		);
-	}
-	
-	elseif(isset($lat) and isset($lon) and $search != '')
-	{
-		$sth = $pdo->prepare('
-		SELECT
-		bedrijfgegevens.bedrijfsnaam,
-		bedrijfgegevens.provincie,
-		bedrijfgegevens.postcode,
-		bedrijfgegevens.plaats,
-		bedrijfgegevens.branche,
-		bedrijfgegevens.premium,
-		bedrijfgegevens.telefoon,
-		bedrijfgegevens.logo,
-		(6371
-		* acos(cos(radians('.$lat.'))
-		* cos(radians(latitude))
-		* cos(radians(longitude)
-		- radians('.$lon.'))
-		+ sin(radians('.$lat.'))
-		* sin(radians(latitude))) 
-		) AS distance,
-		bedrijfs_specialiteiten.*
-	FROM 
-		bedrijfgegevens
-	INNER JOIN
-		bedrijfs_specialiteiten on bedrijfgegevens.bedrijfs_id = bedrijfs_specialiteiten.bedrijfs_id 
-	WHERE 
-		MATCH (bedrijfgegevens.bedrijfsnaam, bedrijfgegevens.postcode, bedrijfgegevens.plaats, bedrijfgegevens.provincie, bedrijfgegevens.branche)
-		AGAINST ("'.$search.'" IN BOOLEAN MODE) 
-		OR MATCH (bedrijfs_specialiteiten.specialiteit_1)
-		AGAINST ("'.$search.'" IN BOOLEAN MODE) 
-		HAVING distance < '.$afstand.'
-		ORDER BY distance ASC
-		LIMIT '.$start.','.$range
-		);
-	}
-	
-	else
-	{
-		$sth = $pdo->prepare('SELECT * FROM bedrijfgegevens ORDER BY premium DESC LIMIT '.$start.','.$range);
-	
-	}
-	$sth->execute();
-
-if(isset($row['distance']))
-{
-$_SESSION['lat'] = $lat;
-$_SESSION['lon'] = $lon;
-}
-$_SESSION['trefwoord'] = $trefwoord;
-$_SESSION['afstand'] = $afstand;
-
-
-
-	echo '<div class="row search-result">';
-		echo '<div class="col-xs-12">';
+	?>
+	<div class="row search-result">
+		<div class="col-xs-12">
+			<?php
 			$rows = 0;
-			while($row = $sth->fetch())
-			{
+			while($row = $sth->fetch()) {
 				$rows++;
-				if($row['premium'] == 'gold')
-				{
-					echo '<a class="greylink" href="bedrijven.php?paginanr=6&bedrijfs_id='.$row['bedrijfs_id'].'">';
-				?>
-					<div class="search-container">
-						<div class="search-image">
-							<img src="images/bedrijf_images/<?php echo $row['bedrijfs_id'].'/'.$row['logo']; ?>" />
-						</div>
-						<span class="glyphicon glyphicon-search premium"></span>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam']. '<br/>' .$row['telefoon']. '<br/>';
-							if(isset($row['distance']))
-							{
-							$round = round($row['distance'], 1);
-							echo number_format($round,1,",",".");
-							}
-							?>
-						</div>
-					</div>
-				<?php
-				echo '</a>';
+
+				if($rows > $range) {
+					break;
 				}
-				elseif($row['premium'] == 'brons')
-				{
-				
-				?>
-					<div class="search-container">
-						<div class="search-image">
-						
-							<span class="glyphicon glyphicon-search no-premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<<?php echo $row['bedrijfsnaam']. '<br/>' .$row['telefoon']. '<br/>';
-							if(isset($row['distance']))
-							{
-							$round = round($row['distance'], 1);
-							echo number_format($round,1,",",".");
-							}
-							?>
-						</div>
-					</div>
-				<?php
+
+				if ($row['premium'] == 'gold') {
+					echo '<a class="greylink" href="bedrijven.php?paginanr=' . $row['nummer'] . '&bedrijfs_id=' . $row['id'] . '" />';
 				}
-				else
-				{
 				?>
-					<div class="search-container">
-						<div class="search-image">
-						
-							<span class="glyphicon glyphicon-search no-premium"></span>
-							<img src="images/truck.jpg">
-						</div>
-						<div class="search-naam">
-							<?php echo $row['bedrijfsnaam'].'<br/>';
-							if(isset($row['distance']))
-							{
-							$round = round($row['distance'], 1);
-							echo number_format($round,1,",",".");
-							}
-							?>
-							km
-						</div>
+				<div class="search-container">
+					<div class="search-image">
+						<?php
+						if ($row['premium'] == 'gold' && !empty($row['logo'])) {
+							echo '<img src="images/bedrijf_images/' . $row['id'] . '/' . $row['logo'] . ' />';
+						} elseif ($row['premium'] != 'gold') {
+							echo '<span class="glyphicon glyphicon-search no-premium"></span>';
+						}
+						?>
 					</div>
+					<div class="search-naam">
+						<?php
+						if ($row['premium'] == 'gold' || $row['premium'] == 'brons') {
+							echo $row['bedrijfsnaam'] . '<br />' . $row['telefoonnummer'] . '<br />';
+						} else {
+							echo $row['bedrijfsnaam'] . '<br />';
+						}
+
+						if (isset($row['distance'])) {
+							$round = round($row['distance'], 1);
+							echo number_format($round, 1, ',', '.') . ' km';
+						}
+						?>
+					</div>
+				</div>
 				<?php
+				if($row['premium'] == 'gold') {
+					echo '</a>';
 				}
-				 
 			}
-			
-		echo '</div>';
-		echo '<div class="col-xs-12">';
-			if($_GET['nr'] == 0)
-				{
-					$nr1 = $_GET['nr'] - 1; 
-					echo '<a href="gids.php?nr='.$nr1.'" class="btn btn-default gids-btn-nav disabled">Terug</a>';
-					
+			?>
+		</div>
+		<div class="col-xs-12">
+			<?php
+			if($nummer == 0) {
+				if($rows > 50) {
+					echo '<a href="gids.php?nr='.($nummer+1).'" class="btn btn-default gids-btn-nav">Volgende</a>';
 				}
-				else
-				{
-					$nr1 = $_GET['nr'] - 1; 
-					echo '<a href="gids.php?nr='.$nr1.'" class="btn btn-default gids-btn-nav">Terug</a>';
+			} else {
+				if($rows > 50 && $nummer > 0) {
+					echo '<a href="gids.php?nr='.($nummer-1).'" class="btn btn-default gids-btn-nav">Terug</a>';
+					echo '<a href="gids.php?nr='.($nummer+1).'" class="btn btn-default gids-btn-nav">Volgende</a>';
+				} else {
+					echo '<a href="gids.php?nr='.($nummer-1).'" class="btn btn-default gids-btn-nav">Terug</a>';
 				}
-			if($rows < 50)
-				{
-					$nr2 = $_GET['nr'] + 1; 
-					echo '<a href="gids.php?nr='.$nr2.'" class="btn btn-default gids-btn-nav disabled">Volgende</a>';
-				}
-			else
-				{
-					$nr2 = $_GET['nr'] + 1; 
-					echo '<a href="gids.php?nr='.$nr2.'" class="btn btn-default gids-btn-nav">Volgende</a>';
-				}
-		echo '</div>';
-	echo '</div>';
-}
+			}
+			?>
+		</div>
+	</div>
+<?php }
 require('./controllers/footer.php');
 ?>
+
 <script>
-analytics.js: ga('send', 'pageview', '/gids.php?q=$trefwoord'); 
+	analytics.js: ga('send', 'pageview', '/gids.php?q=$trefwoord');
 </script>
